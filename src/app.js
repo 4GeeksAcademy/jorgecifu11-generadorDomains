c24b6ddf518e264ffb94599eaeee2c5f1fc91b04
Jorge Cifuentes Pérez <jorge.cifuentes.perez@gmail.com>
2025-05-07 20:17:59 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+function domainGenerator() {
+  let pronouns = ['the', 'our'];
+  let adjetives = ['great', 'big'];
+  let nouns = ['jogger', 'racoon'];
+  let domains = ['.com', '.es']
+  let domainCombinations = []
+
+  for (let pronoun of pronouns){
+    for (let adjetive of adjetives){
+      for (let noun of nouns){
+        for (let domain of domains){
+          domainCombinations.push(${pronoun} ${adjetive} ${noun} ${domain})
+        }
+      }
+    }
+  }
+ return domainCombinations
+}
+let generateDomains = domainGenerator()
+ let domainList = document.querySelector('.domain-generator')
+ generateDomains.map((element) => {
+   domainList.innerHTML += `
+   <li>${element}</li>
+   `
+  })
